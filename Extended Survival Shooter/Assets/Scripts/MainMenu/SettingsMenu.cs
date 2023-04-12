3270853c89b2ba41f3f84c9ee923f5aebee6e865
tastytypist <tastytypist@users.noreply.github.com>
2023-04-12 22:34:49 +0700

    Implement game settings

---
+++ Extended Survival Shooter/Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public InputField NameField;
+    
+    private string _name;
+    private float _volume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _volume = AudioListener.volume;
+        if (_name != "")
+        {
+            NameField.text = _name;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = _volume;
+        if (_name != "")
+        {
+            NameField.text = _name;
+        }
+    }
+
+    public void UpdateName(string username)
+    {
+        _name = username;
+    }
+
+    public void UpdateVolume(float volume)
+    {
+        _volume = volume;
+    }
+}
